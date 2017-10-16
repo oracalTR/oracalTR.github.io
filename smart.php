d1165f84dc504ee1f3113a8d86a4faf9000f8f96
pavel <oracal@mail.ru>
2017-10-16 15:13:51 +0300

    Форма обратной связи

--- smart.php
+++ smart.php
@@ -1,5 +1,11 @@
 <?php 
-
+$your_name = $_POST['your_name'];
+$phone = $_POST['phone'];
+$date = $_POST['date'];
+$time = $_POST['time'];
+$kolper = $_POST['kolper'];
+$email = $_POST['email'];
+$msg = $_POST['msg'];
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
@@ -7,15 +13,15 @@ $mail->CharSet = 'utf-8';
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.timeweb.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'tunerauto@inbox.ru';                 // SMTP username
-$mail->Password = 'iddqdidkfa00';                           // SMTP password
+$mail->Username = 'luhari@luharihookah.ru';                 // SMTP username
+$mail->Password = 'Lcay7tMh';                           // SMTP password
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
 
-$mail->setFrom('tunerauto@inbox.ru', 'Ildar S. Zhiganshin');
-$mail->addAddress('111970@mail.ru', 'Joe User');     // Add a recipient
+$mail->setFrom('luhari@luharihookah.ru', 'Luhari Hookah');
+$mail->addAddress('oracal@mail.ru', 'Luhari Hookah');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -25,14 +31,15 @@ $mail->addAddress('111970@mail.ru', 'Joe User');     // Add a recipient
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Заявка с сайта';
-$mail->Body    = 'Это тело сообщения дальше жирное <b>блаблабла</b>';
+$mail->Body    = "Имя пользователя:".htmlspecialchars($your_name)."<br /> Телефон:".htmlspecialchars($phone)."<br /> Дата:".htmlspecialchars($date)."<br /> Дата: ГГГГ-ММ-ДД <br /> Время:".htmlspecialchars($time)."<br /> Количество персон:".htmlspecialchars($kolper)."<br /> E-mail:".htmlspecialchars($email)."<br /> Сообщение:".htmlspecialchars($msg)."<br />";
 $mail->AltBody = 'Это сообщение в формате plain text';
 
 if(!$mail->send()) {
     echo 'Сообщение не отправлено.';
     echo 'Ошибка: ' . $mail->ErrorInfo;
 } else {
-    echo 'Message has been sent';
+    echo 'Спасибо! Сообщение отправлено.';
+
 }
 
 ?>
